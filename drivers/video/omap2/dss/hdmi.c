c258514d222fa5e9e80969e91d369f7a79f489d2
Jacek Adamski <adams@g.pl>
2013-05-04 11:50:58 +0200

    Merge derp

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -554,10 +554,7 @@ static void hdmi_compute_pll(struct omap_dss_device *dssdev, int phy,
 	 * Input clock is predivided by N + 1
 	 * out put of which is reference clk
 	 */
-	if (dssdev->clocks.hdmi.regn == 0)
-		pi->regn = HDMI_DEFAULT_REGN;
-	else
-		pi->regn = dssdev->clocks.hdmi.regn;
+	pi->regn = dssdev->clocks.hdmi.regn;
 
 	refclk = clkin / (pi->regn + 1);
 
@@ -567,10 +564,7 @@ static void hdmi_compute_pll(struct omap_dss_device *dssdev, int phy,
 	 */
 	pi->regm = (phy * 100 / (refclk)) / 100;
 
-	if (dssdev->clocks.hdmi.regm2 == 0)
-		pi->regm2 = HDMI_DEFAULT_REGM2;
-	else
-		pi->regm2 = dssdev->clocks.hdmi.regm2;
+	pi->regm2 = dssdev->clocks.hdmi.regm2;
 
 	/*
 	 * fractional multiplier is remainder of the difference between
